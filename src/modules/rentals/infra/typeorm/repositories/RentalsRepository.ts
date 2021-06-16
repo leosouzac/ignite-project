5352313622f31b7124831eef0c9fa02ffb79cbef
leonardo <leonardo.coelho@precato.com.br>
2021-06-16 09:13:02 -0300

    Devolution rentals car

--- src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/typeorm/repositories/RentalsRepository.ts
@@ -13,11 +13,15 @@ class RentalsRepository implements IRentalsRepository {
   }
 
   async findOpenRentalByCar(car_id: string): Promise<Rental> {
-    const openByCar = await this.repository.findOne({ car_id });
+    const openByCar = await this.repository.findOne({
+      where: { car_id, end_date: null },
+    });
     return openByCar;
   }
   async findOpenRentalByUser(user_id: string): Promise<Rental> {
-    const openByUser = await this.repository.findOne({ user_id });
+    const openByUser = await this.repository.findOne({
+      where: { user_id, end_date: null },
+    });
     return openByUser;
   }
   async create({
@@ -45,6 +49,15 @@ class RentalsRepository implements IRentalsRepository {
   async findById(id: string): Promise<Rental> {
     const rental = await this.repository.findOne(id);
     return rental;
+  }
+
+  async findByUserId(user_id: string): Promise<Rental[]> {
+    const rentals = await this.repository.find({
+      where: { user_id },
+      relations: ["car"],
+    });
+
+    return rentals;
   }
 }
 
